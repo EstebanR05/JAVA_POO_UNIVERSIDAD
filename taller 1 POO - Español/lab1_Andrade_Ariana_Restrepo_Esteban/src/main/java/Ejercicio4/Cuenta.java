c2bf2b86ac88cce21a796bae5f932bdb8fa2a83b
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2023-09-26 09:01:33 -0500

    se integra funciones de consignar y retirar
    se integra funciones de consignar y retirar

--- taller 1 POO - Español/lab1_Andrade_Ariana_Restrepo_Esteban/src/main/java/Ejercicio4/Cuenta.java
+++ taller 1 POO - Español/lab1_Andrade_Ariana_Restrepo_Esteban/src/main/java/Ejercicio4/Cuenta.java
@@ -27,23 +27,30 @@ public class Cuenta {
     }
 
     public void Consignar(double cantidad) {
-
+        if (this.cantidad >= 0) {
+            this.cantidad += cantidad;
+        }
     }
 
-    public void Retirar(double cantidad) {
-
+    public String Retirar(double cantidad) {
+        if (this.cantidad >= 0) {
+            this.cantidad -= cantidad;
+            return "la cantidad de dinero a entregar es: " + cantidad + " Su cuenta queda con una cantidad de: " + this.cantidad;
+        } else {
+            return "la cantidad en su cuenta es 0, no tienes fondos para retirar: " + cantidad;
+        }
     }
 
     public Cuenta() {
         this.titular = "";
         this.cantidad = 0;
     }
-    
+
     public Cuenta(String titular, int cantidad) {
         this.titular = titular;
         this.cantidad = cantidad;
     }
-    
+
     public Cuenta(Cuenta cuenta) {
         this.titular = cuenta.titular;
         this.cantidad = cuenta.cantidad;
