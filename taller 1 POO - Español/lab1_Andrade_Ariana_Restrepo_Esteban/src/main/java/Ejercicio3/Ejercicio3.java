e843a24bd42efb9460806a9a4a8a980a414db51d
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2023-09-26 08:54:54 -0500

    nuevo cambio

--- taller 1 POO - Español/lab1_Andrade_Ariana_Restrepo_Esteban/src/main/java/Ejercicio3/Ejercicio3.java
+++ taller 1 POO - Español/lab1_Andrade_Ariana_Restrepo_Esteban/src/main/java/Ejercicio3/Ejercicio3.java
@@ -5,25 +5,6 @@ public class Ejercicio3 {
     public static void mostrarEjercicio3() {
         System.out.println("**************************ejercicio 3**********************************");
 
-        Persona persona1 = new Persona();
-        Persona persona2 = new Persona("Esteban", "Restrepo", 1111479211, 'H');
-        Persona persona3 = new Persona(persona2);        
-        
-        //set atributos
-        persona3.setNombre("Ariana");
-        persona3.setApellido("Andrade");
-        persona3.setIdentificacion(0);
-        persona3.setSexo('M');
-     
-        //mostrar estado de las personas
-        System.out.println("El estado de la persona 1 " + persona1.estado());
-        System.out.println("El estado de la persona 2 " + persona2.estado());
-        System.out.println("El estado de la persona 3 " + persona3.estado());
-        
-        //mostrar Si es mayor edad
-        System.out.println("persona 1 " + persona1.esMayorDeEdad());
-        System.out.println("persona 2 " + persona2.esMayorDeEdad());
-        System.out.println("persona 3 " + persona3.esMayorDeEdad());
         
         System.out.println("***********************************************************************");
         System.out.println();
